47a6aa4e4eb39eb023b5ffca8df6340095b79193
Nelven <nelven.thebault@gmail.com>
2025-01-14 16:38:35 +0100

    Ajout code 6axes

---
+++ Software/6axis/Core/Inc/sensor.h
@@ -0,0 +1,13 @@
+/*
+ * Sensor.h
+ *
+ *  Created on: Sep 12, 2024
+ *      Author: nelven
+ */
+
+#ifndef INC_SENSOR_H_
+#define INC_SENSOR_H_
+
+void check_device_communication(void);
+
+#endif /* INC_SENSOR_H_ */
