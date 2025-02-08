1695e1bf91cc9b490fab05c449a49380d6fe8bfe
younsben <younes.benameur@ensea.fr>
2025-02-08 21:04:06 +0100

    servomoteur_ouv_ferm

---
+++ Software/servomoteur/Core/Inc/servoenda.h
@@ -0,0 +1,15 @@
+/*
+ * servoenda.h
+ *
+ *  Created on: Oct 22, 2024
+ *      Author: ybena
+ */
+
+#ifndef INC_SERVOENDA_H_
+#define INC_SERVOENDA_H_
+
+#include <stdio.h>
+void rote(int,int);
+void duty_cycle(int,int);
+
+#endif /* INC_SERVOENDA_H_ */
